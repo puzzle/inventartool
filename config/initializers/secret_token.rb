af427ffba6ee6f43d41e8bfdc6b6309e37597299
Philipp Gassmann <gassmann@puzzle.ch>
2011-02-10 16:31:16 +0100

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InventarTool::Application.config.secret_token = '24a7de6f4b1da33942eb94cbdeecf4213833b5393b93f6c19d57e86e9fc18b93f79731c5014dc77f253783de4b68850bf021fac333cf0eb132d191c5088aefe0'
