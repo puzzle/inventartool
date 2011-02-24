4e9461b48fdaa00a5b1d5fb026ea5aa43615e545
Philipp Gassmann <gassmann@puzzle.ch>
2011-02-24 16:13:39 +0100

    +Routes -public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ InventarTool::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+resources :servers
+resources :rams
   # Sample resource route with options:
   #   resources :products do
   #     member do
