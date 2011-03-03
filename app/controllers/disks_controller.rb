8ef88944d324381726d7b739940c5f2d142f6f52
Philipp Gassmann <gassmann@puzzle.ch>
2011-03-03 16:33:56 +0100

    change selection of machine

--- app/controllers/disks_controller.rb
+++ app/controllers/disks_controller.rb
@@ -1,2 +1,14 @@
 class DisksController < CrudController
+	def edit
+		@machine_types = ["Notebook", "Server"]
+	    @machines = Server.all + Notebook.all
+	    @distributors = Distributor.all
+	    render_with_callback 'edit'
+	end
+	def new
+		@machine_types = ["Notebook", "Server"]
+		@machines = Server.all + Notebook.all
+		@distributors = Distributor.all
+		render_with_callback 'new'
+	end
 end
