88c7173db65064e99f309ef6bbc5aecf015899a2
Philipp Gassmann <gassmann@puzzle.ch>
2011-03-14 12:06:45 +0100

    rebuild versioning view & controller Add and track Creator

--- app/controllers/disks_controller.rb
+++ app/controllers/disks_controller.rb
@@ -7,14 +7,29 @@ class DisksController < CrudController
 	    @distributors = Distributor.all
 	    render_with_callback 'edit'
 	end
+	def update
+	    @entry.attributes = params[model_identifier]
+	    @entry.creator = session[:user_ui]
+	    updated = with_callbacks(:update) { save_entry }
+	    respond_processed(updated, 'updated', 'edit')
+	end
 	def new
 		@machine_types = ["Notebook", "Server"]
 		@machines = Server.all + Notebook.all
 		@distributors = Distributor.all
 		render_with_callback 'new'
 	end
+	def create
+	    @entry.attributes = params[model_identifier]
+	    @entry.creator = session[:user_ui]
+	    created = with_callbacks(:create) { save_entry } 
+	    
+	    respond_processed(created, 'created', 'new') do |format|
+	      format.xml  { render :xml => @entry, :status => :created, :location => @entry } if created
+	    end
+  	end
 	def show
-		@version_diff = diffVersions
+    	@diffhash = diff_hash
     	respond_with @entry
   	end
 	
@@ -26,22 +41,25 @@ class DisksController < CrudController
 		redirect_to :back
 	end
 	
-	def diffVersions
-		set_entry
-		version_diff = []
-		for i in 2..(@entry.versions.count) do
-			v1 = @entry.versions[i - 1]
-			v0 = @entry.versions[i - 2]
-			diff = []
-			for a in 1..(v1.attributes.count) do
+	protected
+	def diff_hash
+		# diff_hash["version"][created_at] = 2011-03-14 08:35:14 UTC
+		# diff_hash["version"]["attribute"] = [ "before", "after" ]
+		diff_hash = Hash.new
+		(2..(@entry.versions.count)).each do |i|
+			v1 = @entry.versions[i - 1] # Aktuelle Version
+			v0 = @entry.versions[i - 2] # Vorherige Version
+			h_version = diff_hash[v1.version] = Hash.new
+			h_version[:created_at] = v1.created_at
+			h_version[:creator] = v1.creator
+			(1..(v1.attributes.count)).each do |a|
 				a1 = v1.attributes.to_a[a]
 				a0 = v0.attributes.to_a[a]
 				if a0.to_a[1] != a1.to_a[1] && !(["updated_at", "version", "id"].include? a0[0] )
-					diff << [a1[0],a0[1], a1[1]]
+					h_version[a1[0]] = [a0[1], a1[1]] # 
 				end
 			end
-			version_diff << diff
 		end
-		version_diff
+		diff_hash
 	end
 end
