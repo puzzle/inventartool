f76b0b2fbf32e2e5bfa7c98fb3d4b2b6b7818284
Philipp Gassmann <gassmann@puzzle.ch>
2011-03-24 08:53:35 +0100

    fix migrations, add machine_type was merged into create_rams, but has not been deleted

--- db/migrate/20110224160822_add_machine_type_to_rams.rb
+++
@@ -1,9 +0,0 @@
-class AddMachineTypeToRams < ActiveRecord::Migration
-  def self.up
-  	add_column :rams, :machine_type, :string
-  end
-
-  def self.down
-  	remove_column :rams, :machine_type, :string
-  end
-end
