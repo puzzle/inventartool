4f5238fd7309067216c3c0167f9729ef6ad13b24
Roman Alt <ralt@ralt-puzzle.(none)>
2011-03-25 10:30:14 +0100

    Warranty Calculator fine

--- app/controllers/warranty_controller.rb
+++ app/controllers/warranty_controller.rb
@@ -1,9 +1,11 @@
 class WarrantyController < ApplicationController
 	#@warranty_end muss vom User definiert werden können durch Date-Formular.
 	def index
-		@warranty_end = 14
-		@warranty_object = :warranty_till
-		@notebooks = Notebook.where("warranty_till <= :end_date", :end_date => (Date.today + @warranty_end.day))
+		if params[:warranty_end_date] == nil
+		@notebooks = Notebook.all
+		else
+		@notebooks = Notebook.where("warranty_till <= :end_date", :end_date => params[:warranty_end_date])
 	end
+	end	
 	
 end
