8c3527ad26eb7591954664d31c73f134fdc2c691
Roman Alt <ralt@ralt-puzzle.(none)>
2011-03-28 09:42:06 +0200

    Include Crud Table Include all Objects Include low Warranty field

--- app/controllers/warranty_controller.rb
+++ app/controllers/warranty_controller.rb
@@ -1,13 +1,25 @@
-class WarrantyController < ApplicationController
+class WarrantyController < CrudController
 	#@warranty_end muss vom User definiert werden können durch Date-Formular.
 	def index
-		if params[:warranty_end_date] == ""
-			params[:warranty_end_date] = nil
+		@low_warranty = params[:lowest_warranty_end_date]
+		@warranty = params[:warranty_end_date]
+		if @warranty == ""
+			if @low_warranty == ""
+			@warranty = nil
+			else
+				@warranty = Date.today
+			end
 		end
-		if params[:warranty_end_date] == nil
+		if @warranty == nil
 		@notebooks = Notebook.all
+		@servers = Server.all
+		@disks = Disk.all
+		@rams = Ram.all
 		else
-		@notebooks = Notebook.where("warranty_till <= :end_date", :end_date => params[:warranty_end_date])
+		@notebooks = Notebook.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => @warranty, :low_date => @low_warranty)
+		@servers = Server.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => @warranty, :low_date => @low_warranty)
+		@disks = Disk.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => @warranty, :low_date => @low_warranty)
+		@rams = Ram.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => @warranty, :low_date => @low_warranty)
 	end
 	end	
 	
