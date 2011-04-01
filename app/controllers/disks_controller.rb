0b6338d5cd1e41991b6ea5a17b46a81a1b91a315
Roman Alt <ralt@ralt-puzzle.(none)>
2011-04-01 08:49:37 +0200

    Delete Logger Messages Use standard crud form

--- app/controllers/disks_controller.rb
+++ app/controllers/disks_controller.rb
@@ -17,8 +17,9 @@ class DisksController < CrudController
 	    	@a_machines << ["Server: #{machine.label}", "Server_#{machine.id}"]
 	    end
 	    @a_machines << ["(none)", nil ]
-	    
-	    @entry.warranty_till = ((@entry.warranty_till - @entry.purchase_date).to_i)/365
+	    if (@entry.warranty_till != nil)
+	    	@entry.warranty_till = ((@entry.warranty_till - @entry.purchase_date).to_i)/365
+	    end
 	end
 
 	def set_machine
@@ -41,13 +42,9 @@ class DisksController < CrudController
 	end
 	
 	def set_warranty
-		@intI = (params[model_identifier][:warranty_till]).to_i
-		@intY = Date.parse( {"1i"=>params[model_identifier][:"purchase_date(1i)"], "2i"=>params[model_identifier][:"purchase_date(2i)"], "3i"=>params[model_identifier][:"purchase_date(3i)"]}.to_a.sort.collect{|c| c[1]}.join("-"))
-		RAILS_DEFAULT_LOGGER.error("Test Here " + (@intI).to_s)
-		RAILS_DEFAULT_LOGGER.error("Test Here " + (@intY).to_s)
-		puts @intI
-		puts @intY
-		params[model_identifier][:warranty_till] = @intY + @intI.years
+		@warranty_till = (params[model_identifier][:warranty_till]).to_i
+		@purchase_date = Date.parse( {"1i"=>params[model_identifier][:"purchase_date(1i)"], "2i"=>params[model_identifier][:"purchase_date(2i)"], "3i"=>params[model_identifier][:"purchase_date(3i)"]}.to_a.sort.collect{|c| c[1]}.join("-"))
+		params[model_identifier][:warranty_till] = @purchase_date + @warranty_till.years
 	end
 	
 	def detach
