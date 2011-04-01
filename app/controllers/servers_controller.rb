9474ebb5ba540182b25e2b602c53fcd717087df2
Roman Alt <ralt@ralt-puzzle.(none)>
2011-04-01 08:53:49 +0200

    New _form.html.erb for Notebook + Server Add Warranty_calculation in Notebooks- + Servers_controller

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,3 +1,20 @@
 class ServersController < CrudController
 	self.search_columns = [:model, :processor, :serial_number, :notes]
+	before_filter :set_warranty, :only => [:update, :create]
+	before_render_form :set_values
+	
+	def set_warranty
+		@owners = Owner.all
+		
+		@warranty_till = (params[model_identifier][:warranty_till]).to_i
+		@purchase_date = Date.parse( {"1i"=>params[model_identifier][:"purchase_date(1i)"], "2i"=>params[model_identifier][:"purchase_date(2i)"], "3i"=>params[model_identifier][:"purchase_date(3i)"]}.to_a.sort.collect{|c| c[1]}.join("-"))
+		params[model_identifier][:warranty_till] = @purchase_date + @warranty_till.years
+	end
+	
+	def set_values
+		
+		if (@entry.warranty_till != nil)
+	    	@entry.warranty_till = ((@entry.warranty_till - @entry.purchase_date).to_i)/365
+	    end
+	end
 end
