c7a5f4566afeafe77f4ed817a65b245eb30e98bc
Roman Alt <ralt@ralt-puzzle.(none)>
2011-04-01 10:05:55 +0200

    Two new Attributes: Age and Warranty rest new partial for warranty Calculations Reference from notebook show to new partial

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -8,4 +8,12 @@ class Notebook < ActiveRecord::Base
   def label
   	"#{model}"
   end
+  
+  def age_in_days
+  	(Date.today - purchase_date).to_s
+  end
+  
+  def warranty_end_in_days
+  	(warranty_till - Date.today).to_s
+  end
 end
