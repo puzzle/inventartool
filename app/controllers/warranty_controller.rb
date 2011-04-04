1c57713ec754068ab68571c966d843fa32ffd44c
Roman Alt <alt@puzzle.ch>
2011-04-04 15:22:39 +0200

    little changes on warranty Calculator

--- app/controllers/warranty_controller.rb
+++ app/controllers/warranty_controller.rb
@@ -1,23 +1,23 @@
 class WarrantyController < CrudController
 	#@warranty_end muss vom User definiert werden können durch Date-Formular.
 	def index
-		@low_warranty = params[:lowest_warranty_end_date]
-		@warranty = params[:warranty_end_date]
-		if @warranty == ""
-			if @low_warranty == ""
-			@warranty = nil
+		low_warranty = params[:lowest_warranty_end_date]
+		max_warranty = params[:warranty_end_date]
+		if max_warranty == ""
+			if low_warranty == ""
+			max_warranty = nil
 			else
-				@warranty = "9999-99-99"
+				max_warranty = "9999-99-99"
 			end
 		end
-		if @warranty == nil
+		if max_warranty == nil
 			@notebooks = Notebook.all
 			@servers = Server.all
 			@disks = Disk.all
 		else
-			@notebooks = Notebook.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => @warranty, :low_date => @low_warranty)
-			@servers = Server.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => @warranty, :low_date => @low_warranty)
-			@disks = Disk.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => @warranty, :low_date => @low_warranty)
+			@notebooks = Notebook.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => max_warranty, :low_date => low_warranty)
+			@servers = Server.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => max_warranty, :low_date => low_warranty)
+			@disks = Disk.where("warranty_till <= :end_date AND warranty_till >= :low_date", :end_date => max_warranty, :low_date => low_warranty)
 		end
 	end
 	
