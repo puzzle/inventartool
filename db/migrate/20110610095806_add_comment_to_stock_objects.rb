4a5e5b19f22a86ffd29d5de8d832458a7c541c65
Philipp Gassmann <gassmann@puzzle.ch>
2011-06-10 12:32:30 +0200

    change migration to also include versioned table

--- db/migrate/20110610095806_add_comment_to_stock_objects.rb
+++ db/migrate/20110610095806_add_comment_to_stock_objects.rb
@@ -1,9 +1,11 @@
 class AddCommentToStockObjects < ActiveRecord::Migration
   def self.up
     add_column :stock_objects, :comment, :text
+    add_column :stock_object_versions, :comment, :text
   end
 
   def self.down
     remove_column :stock_objects, :comment
+    remove_column :stock_object_versions, :comment
   end
 end
